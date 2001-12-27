bbbbccf7d047dd2cef0cb1f7ae6153955d22c600
gskc <gskc>
2001-12-27 20:22:10 +0000

    todo: find real WF-Name, and 'prefix'
    NRLProcessor: now performs WFLoader's task of loading NRL-spec files from file
    system

--- proc/nrl/NRLProcessor.java
+++ proc/nrl/NRLProcessor.java
@@ -8,15 +8,23 @@ import psl.survivor.util.*;
 
 import wfruntime.Output;
 import wfruntime.IScheduler;
+import wfruntime.SpecParser;
 import wfruntime.ConfigInfo;
 import wfruntime.RoutingInfo;
+import wfruntime.IServiceHost;
 import wfruntime.Scheduler_Serv;
 import wfruntime.ServiceHost_Serv;
 import wfruntime.IRealizationInfo;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.FileInputStream;
+
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Hashtable;
 
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 
 public class NRLProcessor extends Processor {
@@ -25,12 +33,22 @@ public class NRLProcessor extends Processor {
   private Scheduler_Serv _scheduler;
   private final Hashtable _resultDataStorage = new Hashtable();
 
+  private final String wfRootDir;
+  private final String wfName;
+  private final String namePrefix;
+  
+  private final int rmiPort;
+
   // BEGIN: Inherited from psl.survivor.proc.Processor /////////////////////////
   public NRLProcessor(String name, int tcpPort, String rmiName, 
                       String wfDefPath) {
       super(name, tcpPort, rmiName, wfDefPath);
+
+      wfRootDir = wfDefPath;
+      wfName = "A_WORKFLOW"; // todo: need to get real wfName
+      namePrefix = wfName; // todo: what is this really supposed to be?
       
-      final int rmiPort = WVM_Host.PORT;
+      rmiPort = WVM_Host.PORT;
       // BEGIN: Inherited from wfruntime.ServiceHost_Serv ///
       try {
 	  _serviceHost = new ServiceHost_Serv(rmiPort, wfDefPath) {
@@ -102,16 +120,25 @@ public class NRLProcessor extends Processor {
     String originTask = processData.originTask;
     String state = processData.state;
     Hashtable paramTable = processData.paramTable;
+    String thisTaskName = processData.nextTaskName;
 
     // TODO need to fix the instanceId to be unique per task, not workflow
     try {
+      resetTask(thisTaskName); // copied from WFLoader
+      // this will internally call beginScheduler(...)
+  
       _scheduler.transition(instanceId, originTask, state, paramTable);
     } catch (RemoteException re) {
       // left here for backwards compatibility
+      // possibly thrown by: _scheduler.transition(instanceId, originTask, state, paramTable);
+    } catch (Throwable t) {
+      // left here for backwards compatibility
+      // possibly thrown by: resetTask(thisTaskName);
     }
 
     NRLProcessData resultData
       = (NRLProcessData) _resultDataStorage.get(instanceId.toString());
+    _resultDataStorage.remove(instanceId.toString());
 
     TaskDefinition td = new TaskDefinition(resultData.nextTaskName);
     td.addRequirement(new NameValuePair("tn", resultData.nextTaskName));
@@ -122,4 +149,109 @@ public class NRLProcessor extends Processor {
   }
   // ENDED: Inherited from psl.survivor.proc.Processor /////////////////////////
 
+  // BEGIN: Copied from wfruntime.psl.PSLWFLoader //////////////////////////////
+  /**
+   * resetTask copied over from WFLoader ... 
+   * this will load NRL-defined spec files from the local filesystem
+   * and internally call beginScheduler(...) on the local scheduler
+  */
+  public void resetTask(String taskName) throws Throwable {
+    final char sc = File.separatorChar;
+    File              subDir      = new File(wfRootDir,"workflows"+sc+wfName+sc+taskName);
+    RoutingInfo       theRouting  = SpecParser.parseRouting(new File(subDir,"routing"));
+    HashSet           thePerms    = SpecParser.parseDataPerms(new File(subDir,"dataperm"));
+    Hashtable         creates     = SpecParser.parseCreates(new File(subDir,"creates"));
+    Hashtable         dTypes      = SpecParser.parseDataTypes(new File(subDir,"datatypes"));
+    Hashtable         exceptions  = SpecParser.parseExceptions(new File(subDir,"exceptions"));
+    Hashtable         fTypes      = SpecParser.parseFieldTypes(new File(subDir,"fieldtypes"));
+    IRealizationInfo  rinfo       = SpecParser.parseRealization(new File(subDir,"realization"));
+
+    IServiceHost theHost = (IServiceHost) Naming.lookup("rmi://localhost:" + rmiPort + "/serviceHost");
+    ConfigInfo theConfig = SpecParser.parseConfig(new File(wfRootDir, "workflows" + sc + wfName + sc + "config"));
+
+    if (theRouting.getTaskType().equals("NonTransactionalTaskRealization") ||
+      theRouting.getTaskType().equals("SynchronizationTaskIn") ||
+      theRouting.getTaskType().equals("SynchronizationTaskOut")) {
+      String[] files = subDir.list(new FilenameFilter() {
+        public boolean accept(File file, String name) {
+          return name.endsWith(".class");
+        }
+      });
+      
+      for (int i=0; i<files.length; i++)
+        theHost.putRealizationInClasspath(wfName,taskName,files[i], readClassFile(new File(subDir,files[i])));
+    }
+    
+    try {
+      if (theRouting.getTaskType().equals("SynchronizationTaskIn")) {
+        /*
+         * // Not implemented: 27 Dec 2001
+        LOG.println("starting syncin");
+        theHost.beginSyncInManager(taskName,theRouting,thePerms,creates,dTypes,namePrefix,theConfig,fTypes);
+        */
+      } else {
+        /*
+         * // Not implemented: 27 Dec 2001
+        if (theRouting.getTaskType().equals("NonTransactionalNetwork")) {
+          PSLWFLoader newLoader = new PSLWFLoader(wfRootDir.getCanonicalPath(),
+                                                  theRouting.getNestedWFName(),
+                                                  namePrefix + "." + taskName,
+                                                  regPort,
+                                                  taskHostName);
+          newLoader.startSchedulers();
+          WFTaskRealizationInfo wfrealinfo = (WFTaskRealizationInfo) rinfo;
+          wfrealinfo.setStartHost(newLoader.getConfig().getHostForTask(wfrealinfo.getStartTask()));
+        }
+        */
+        theHost.beginScheduler(taskName,theRouting,thePerms,creates,dTypes,exceptions,namePrefix,theConfig,fTypes,rinfo);
+     }
+    } catch (Throwable t) {
+      t.printStackTrace();
+    }
+    
+    // send data classFiles
+    Object[] createParams = creates.keySet().toArray();
+    String className;
+    FileInputStream fileIn;
+    HashSet classesToLoad = new HashSet();
+    
+    for (int p=0;p<createParams.length;p++) {
+      className = (String) creates.get(createParams[p]);
+      classesToLoad.add(className);
+    }
+    
+    Object[] useParams = dTypes.keySet().toArray();
+    for (int p=0;p<useParams.length;p++) {
+      className = (String) dTypes.get(useParams[p]);
+      classesToLoad.add(className.substring(0,className.lastIndexOf(".")+1)+"I"+className.substring(className.lastIndexOf(".")+1));
+      classesToLoad.add(className+"_Stub");
+    }
+    
+    Object[] exceps = exceptions.keySet().toArray();
+    for (int p=0;p<exceps.length;p++) {
+      classesToLoad.add(exceps[p]);
+    }
+    // System.out.println("GET RID OF ME! classesToLoad.size(): " + classesToLoad.size());
+
+    Iterator classIter = classesToLoad.iterator();
+    while (classIter.hasNext()) {
+      className = (String)classIter.next();
+      // theHost.putDataClassInClasspath(className,readClassFile(new File(wfRootDir.getCanonicalPath()+File.separatorChar+"data"+File.separatorChar+className.replace('.','\\')+".class")));
+      // System.out.println("PSL! change");        theHost.putDataClassInClasspath(className,readClassFile(new File(wfRootDir.getCanonicalPath()+File.separatorChar+"data"+File.separatorChar+className.replace('.',File.separatorChar)+".class")));
+      // todo: why aren't we sending the class files over?
+      
+    }
+    // new for loop/ send new configs
+  }
+
+  private static byte[] readClassFile(File theFile) throws Throwable {
+    FileInputStream in = new FileInputStream(theFile);
+    byte[] returnObj = new byte[(int)theFile.length()];
+    in.read(returnObj);
+    in.close();
+    return returnObj;
+  }
+
+  // ENDED: Copied from wfruntime.psl.PSLWFLoader //////////////////////////////
+ 
 }
